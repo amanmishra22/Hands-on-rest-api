759ec1bbb853f5140be6de7000ba013f2a1d3300
Aman Mishra <amanamanram108@gmail.com>
2024-06-02 22:39:12 +0530

    latest

--- start.js
+++ start.js
@@ -2,3 +2,23 @@ const express= require('express');
 const Joi=require('joi');
 const app=express();
 app.use(express.json());
+
+const customers=[
+    {title: 'Ram', id: 1},
+    {title: 'Shyam', id: 2},
+    {title: 'Mohan', id: 3},
+    {title: 'Sohan', id: 4}
+]
+app.get('/',(req,res)=>{
+    res.send('Welcome');
+});
+app.get('/api/customers',(req,res)=>{
+    res.send(customers);
+});
+app.get('/api/customers/:id',(req,res)=>{
+     const customer=customers.find(c=>c.id===parseInt(req.params.id));
+     if(!customer)res.status(404).send('<h2>Not found</h2>');
+     res.send(customer);
+});
+
+
