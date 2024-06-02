996d22d5f924ad5da89436eeeb38c5a33f102a2e
Aman Mishra <amanamanram108@gmail.com>
2024-06-02 23:43:38 +0530

    added put and delete

--- start.js
+++ start.js
@@ -20,6 +20,36 @@ app.get('/api/customers/:id',(req,res)=>{
      if(!customer)res.status(404).send('<h2>Not found</h2>');
      res.send(customer);
 });
+app.post('/api/customers',(req,res)=>{
+    // const {error}=validateCustomer(req.body);
+    // if(error){
+    //     res.status(400).send('<h2>Error</h2>');
+    //     return;
+    // }
+    const customer={
+        id: customers.length+1,
+        title: req.body.title
+    };
+    customers.push(customer);
+    res.send(customers);
+});
+app.put('/api/customers/:id',(req,res)=>{
+    const customer=customers.find(c=>c.id===parseInt(req.params.id));
+    if(!customer){
+        res.status(400).send('<h2>Error</h2>');
+    }
+    customer.title=req.body.title;
+    res.send(customer);
+});
+app.delete('/api/customers/:id',(req,res)=>{
+    const customer=customers.find(c=>c.id===parseInt(req.params.id));
+    if(!customer){
+        res.status(400).send('<h2>Error</h2>');
+    }
+    const index=customers.indexOf(customer);
+    customers.splice(index,1);
+    res.send(customers);
+});
 function validateCustomer(customer){
     const schema={
         title:Joi.string().min(3).required()
